862f518daec6e944a74de8fdb3925d0a9bedfe65
weitude <eric.c.w.lin@gmail.com>
2022-06-09 11:56:51 +0800

    change epoch from 3000 to 10000, and add requirements.txt

--- ml.py
+++ ml.py
@@ -198,7 +198,7 @@ config = {
     'seed': 5602,  # seed number
     'select_all': False,  # Whether to use all features.
     'valid_ratio': 0.2,  # validation_size = train_size * valid_ratio
-    'n_epochs': 3000,  # Number of epochs.
+    'n_epochs': 10000,  # Number of epochs.
     'batch_size': 256,
     'learning_rate': 1e-5,
     'early_stop': 1000,  # If model has not improved for this many consecutive epochs, stop training.
